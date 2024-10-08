35079f8ce3913cb62213f3f30d5e68efc699ecde
Gabriel Walter Ysidro Palomino <20222848@aloe.ulima.edu.pe>
2024-10-08 16:09:01 -0500

    Co-authored-by: kerumandev <W1llpr0@users.noreply.github.com>

--- src/logic/Focussed DStar.js
+++ src/logic/Focussed DStar.js
@@ -1,10 +1,11 @@
 import Cola from "./Cola.js"
 
 const Initialize = () => {
+    //Inicializacion de colas de prioridad para cada fila y columna
     const ColaRow = new Cola();
     const ColaCol = new Cola();
 
-    
+
 }
 
 export function FocussedDStar () {
